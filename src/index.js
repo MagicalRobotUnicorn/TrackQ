f23dce3eadf8c0ccc7cacdbf5d63502dfe1181d3
John Weber <johnhenryweber@gmail.com>
2019-12-19 08:17:27 -0800

    Added Global State with Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Spinner from './Spinner';
+import firebase from './firebase';
+import { setUser } from './actions/';
 
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
 
-const Root = () => (
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends Component {
+  componentDidMount() {
+    // console.log(this.props.isLoading);
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      }
+    })
+  }
+  render() {
+    return this.props.isLoading ? <Spinner /> : (
   <Router>
     <Switch>
       <Route exact path="/" component={App} />
@@ -17,8 +39,26 @@ const Root = () => (
       <Route path="/register" component={Register} />
     </Switch>
   </Router>
-);
+    )
+  }
+}
+
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(
+    mapStateFromProps,
+    { setUser }
+    )(Root)
+  );
+
+ReactDOM.render(
+<Provider store={store}>
+<Router>
+<RootWithAuth/>
+</Router>
+</Provider>, document.getElementById('root'));
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-
-
